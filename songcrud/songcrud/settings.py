1b8fcbaa6e93d329b8cea09731697edff5ec917e
Ichiomacode <ichiomafaustina@gmail.com>
2022-10-22 00:22:33 +0300

    Alas-my first django project

--- songcrud/songcrud/settings.py
+++ songcrud/songcrud/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #added an application created by me
+    'musicapp',
 ]
 
 MIDDLEWARE = [
